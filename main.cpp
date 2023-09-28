16b2c97829bdb00e8f9da66fd4aba53e8018f3d1
pthaiodayne <phuthai3009@gmail.com>
2023-09-28 10:17:27 +0700

    hoàn thiện

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ int main() {
     cout << "Ten ban la gi: ";
     getline (cin, name);
     cout << "hello " << name;
-    cout << "ban bao nhieu tuoi: ";
+    cout << "\n ban bao nhieu tuoi: ";
     cin >> n;
-    if (n>=18)(
+    if (n>=18){
         cout << "chao mung ban den voi chung toi";}
     else cout << "ban khong du tuoi";
     return 0;
