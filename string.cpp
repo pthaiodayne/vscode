11ba607c99ad3a0982688252563ab2421e8e867a
pthaiodayne <phuthai3009@gmail.com>
2023-09-28 10:35:35 +0700

    đếm số từ chuẩn hơn

--- string.cpp
+++ string.cpp
@@ -11,22 +11,27 @@ int main() {
 #include <iostream>
 #include <iomanip>
 using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <string> // Added this line to include the <string> header file
+using namespace std;
+
 int main()
 {
     string s;
     int n=0;
-cout << setw(4) << 6 << endl
-<< setw(4) << 18 << endl
-<< setw(4) << 124 << endl
-<< "---\n"
-<< setw(4)<< (6+18+124) << endl;
-cout << "nhap ho va ten cua ban: ";
+    cout << setw(4) << 6 << endl
+         << setw(4) << 18 << endl
+         << setw(4) << 124 << endl
+         << "---\n"
+         << setw(4)<< (6+18+124) << endl;
+    cout << "nhap ho va ten cua ban: ";
     getline(cin,s);
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] >= 'A' && s[i] <= 'Z') {
-			n++;
-		}
-	}
-	cout << n;
-return 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            n++;
+        }
+    }
+    cout << "so tu la: " << n;
+    return 0;
 } 
