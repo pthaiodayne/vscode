df0a1d65a82c8fb7cf8e28680c98e779fedd0eff
pthaiodayne <145412041+pthaiodayne@users.noreply.github.com>
2023-09-28 10:09:10 +0700

    update thêm tuổi

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include <string>
 using namespace std;
 int main() {
+    int n;
     string name;
     cout << "hello friend" << "\n";
     cout << "Ten ban la gi: ";
     getline (cin, name);
     cout << "hello " << name;
+    cout << "ban bao nhieu tuoi: ";
+    cin >> n;
+    if (n>=18)(
+        cout << "chao mung ban den voi chung toi";}
+    else cout << "ban khong du tuoi";
     return 0;
 }
